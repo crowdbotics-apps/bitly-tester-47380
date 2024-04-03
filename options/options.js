6eae4fdf142c6da0033548f3b662902add31f375
Crowdbotics <team@crowdbotics.com>
2024-04-03 16:30:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitly-tester-47380",
+  url: "https://bitly-tester-47380.botics.co",
+  api: "https://bitly-tester-47380.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
